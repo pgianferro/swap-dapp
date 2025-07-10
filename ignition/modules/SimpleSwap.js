3bc55f59655f9b23af1b7a683a6fcc23fa4f16fe
pgianferro <pablogianferro@gmail.com>
2025-07-10 10:56:09 -0300

    Re-deployed tokens and contract, updated frontend with working swap

--- ignition/modules/SimpleSwap.js
+++ ignition/modules/SimpleSwap.js
@@ -2,8 +2,8 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 const SimpleSwapModule = buildModule("SimpleSwapModule", (deployer) => {
-  const tokenA = "0xd8A4f6679C57E5afe67fB495eEB7f7E1897e08ec"; 
-  const tokenB = "0x0B3F610367fd1b7A85Bc83415fdB6A2674E22C0E";
+  const tokenA = "0xE893eB464b251d3174f4E2210aB537918cC4FFF6"; 
+  const tokenB = "0x18d27f72Bc6B7CE5cf1998d4e4fA746630cA3C40";
 
   const swap = deployer.contract("SimpleSwap", [tokenA, tokenB]);
 
